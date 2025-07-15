e7f0590821f90a0f8cbd108fd1285ddc4c94ae99
Sumukha-S-Hebbar <120936975+Sumukha-S-Hebbar@users.noreply.github.com>
2025-07-15 18:51:03 +0000

    till 12

--- basics/12_numbers_and _maths.js
+++
@@ -1,2 +0,0 @@
-const num1= new Number(10); // Number object
-console.log(num1.toString.[0])
---
+++ basics/12_numbers_and_maths.js
@@ -0,0 +1,7 @@
+const num1= new Number(10); // Number object
+console.log(num1.toString())
+const number =1000000
+console.log(number.toLocaleString());
+
+
+//learn about math and number methods
