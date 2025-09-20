9a4f015c96320e82e2eabf095871a42e6c0ac309
Sumukha-S-Hebbar <120936975+Sumukha-S-Hebbar@users.noreply.github.com>
2025-09-20 08:08:49 +0000

    till 32

---
+++ 04_dom/01_dom.js
@@ -0,0 +1,27 @@
+//first thing is we will get the window
+console.log(Window);
+//then inside the window we will get document
+console.log(Window.document);
+//we can also give it as 
+console.log(document)
+//if we want other properties of documents then we can use
+console.dir(document)
+//we can select the elements in different ways 
+document.getElementById()
+document.getElementsByClassName()
+//while writing html we write calss but it is read as classname by document
+//then after getting the element we can get the attributes
+document.getElementById().getAttribute()
+// document.getElementById().setAttribute(attribute name,attribute value)
+//the above thing will overwrite
+//there are differneces between innerHTML, innerText, TextContent
+//innerHTML will give the tags inside
+//innerText will display only the text that is visible
+//TextContent will display all the content inisde except tags 
+//queryselector
+//queryselector all
+
+
+
+
+
